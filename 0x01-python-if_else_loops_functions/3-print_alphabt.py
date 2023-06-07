7e1b937a04870b188dc7fda278a46f3840a78516
Serinajefthas <serina.jefthas@gmail.com>
2023-06-07 15:30:58 -0700

    Fixed pycodestyle erros

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if (chr(i) == 'p' or chr(i) == 'e'):
+    if chr(i) == 'p' or chr(i) == 'e':
         i += 1
     else:
-        print("{:c}".format(i), end = "")
+        print("{:c}".format(i), end="")
