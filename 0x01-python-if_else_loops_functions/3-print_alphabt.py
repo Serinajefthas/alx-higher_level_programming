42c7997b6a0597eab5afcfdf9929c232593d9cb1
Serinajefthas <serina.jefthas@gmail.com>
2023-06-07 15:35:01 -0700

    error checking

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'p' or chr(i) == 'e':
+for i in range(97, 123):
+    if i == 101 or i == 113:
         i += 1
     else:
         print("{:c}".format(i), end="")
