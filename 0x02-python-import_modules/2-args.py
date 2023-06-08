a3158a1ca98799fc9dbc42aa7bf998c0de38e9c4
Serinajefthas <serina.jefthas@gmail.com>
2023-06-08 14:49:36 -0700

    Check that module run as main script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("1 argument:")
-    print("1: {}".format(sys.argv))
-else:
-    print("{:d} arguments:".format(len(sys.argv)))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: {:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv)))
+        for i in range(len(sys.argv) - 1):
+            print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
