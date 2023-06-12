afff8badf16b2edf1c2483e3219748b1b672842b
Serinajefthas <serina.jefthas@gmail.com>
2023-06-12 08:12:10 -0700

    Add tasks 1-5

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(
+                print("{}".format(matrix[i][j]))
+        print("\n")
