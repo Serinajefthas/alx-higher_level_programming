13517e96b4ba2769550bf5170ed821e1b183c43b
Serinajefthas <serina.jefthas@gmail.com>
2023-06-13 16:49:19 -0700

    Update format() print method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        for i in reversed(range(len(my_list))):
-            print("{}".format(my_list[i]))
-    else:
-        return
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for element in range(len(matrix[row])):
+    for row in matrix:
+        for element in row:
             if element == len(matrix[row]) -1:
                 print("{}".format(matrix[row][element]), end="")
             else:
