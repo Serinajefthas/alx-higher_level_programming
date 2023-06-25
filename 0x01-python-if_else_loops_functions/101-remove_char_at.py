1d8d11003abd1c4707ad9dc115b572b9feecd8d9
Serinajefthas <serina.jefthas@gmail.com>
2023-06-25 14:48:23 -0700

    Update incorrect output adv tasks

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,8 +3,6 @@ def remove_char_at(str, n):
     new_str = ''
     if n < 0:
         return (str)
-    for i in str:
-        if i == n:
-            i += 1    
-        new_str += chr(i)
-    return (new_str)
+    else:
+        new_str = str[:n] + str[n + 1:]
+        return (new_str)
