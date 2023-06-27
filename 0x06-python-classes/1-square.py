6b924ebc50b21a65b5de5e13eb972874b6b846f0
Serinajefthas <serina.jefthas@gmail.com>
2023-06-27 14:21:39 -0700

    Add comments to tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-class Square:
-
+""" class Square that defines a square"""
+class Square():
+    """ Initialise class"""
     def __init__(self, size):
+        """Args:
+           size (int): size of the square
+        """
        self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+""" class Square defines a square"""
+
 class Square:
+    """ class Square that defines a square"""
 
     def __init__(self, size=0):
+        """Initialize square
+        Args:
+            size (int): size of the square
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
