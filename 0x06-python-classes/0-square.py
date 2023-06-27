25ff2ff46190e4630cdb748ee6f821c0d8535d7e
Serinajefthas <serina.jefthas@gmail.com>
2023-06-27 11:58:26 -0700

    Add readme and task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    length = None
+    width = None
+
+    def _init_(self, length, width):
+       self.length = length
+       self.width = width
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+Projects on Object Oriented Programming, including classes, attributes, private vs public etc in Python
