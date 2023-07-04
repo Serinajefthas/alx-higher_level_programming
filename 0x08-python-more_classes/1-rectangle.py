9edbb7d00b51b00a2f6b7781770ea08f27ae4d7c
Serinajefthas <serina.jefthas@gmail.com>
2023-07-04 05:59:41 -0700

    Add tasks 0-3

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """Initialise new square
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        """Get method to retrieve width"""
+        return (self._width)
+
+    @width.setter
+    def width(self, value):
+        """Setter to set value of width with exceptions"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    
+    @property
+    def height(self):
+        """Get method to retrieve height"""
+        return (self._height)
+
+    @height.setter
+    def height(self, value):
+        """Setter to set value of height with exceptions"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        self._height = value
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Project OOP tasks on more classes, objects, instance variables & methods and __str__ and __repr__ etc methods in Python
