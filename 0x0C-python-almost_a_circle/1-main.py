6a53683128b133837fdf82a96c49fcf64b4c0613
Serinajefthas <serina.jefthas@gmail.com>
2023-07-21 05:06:10 -0700

    Update syntax error in square to dict method

--- 0x0C-python-almost_a_circle/1-main.py
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-""" 9-main """
+""" 13-main """
 from models.square import Square
 
 if __name__ == "__main__":
 
-    s1 = Square(5)
+    s1 = Square(10, 2, 1)
     print(s1)
-    print(s1.area())
-    s1.display()
+    s1_dictionary = s1.to_dictionary()
+    print(s1_dictionary)
+    print(type(s1_dictionary))
 
-    print("---")
-
-    s2 = Square(2, 2)
+    s2 = Square(1, 1)
     print(s2)
-    print(s2.area())
-    s2.display()
-
-    print("---")
-
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+    s2.update(**s1_dictionary)
+    print(s2)
+    print(s1 == s2)
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -67,5 +67,5 @@ class Square(Rectangle):
             "id": self.id,
             "size": self.width,
             "x": self.x,
-            "y", self.y,
+            "y": self.y
         }
