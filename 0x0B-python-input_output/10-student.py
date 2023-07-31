2a21ee496adeead4bad18a5a4c135fd04e85f1d6
Serinajefthas <serina.jefthas@gmail.com>
2023-07-31 14:51:01 -0700

    Add task 9 and 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module defining the class Student based on 9-student.py"""
+
+
+class Student:
+    """Class that defines properties of student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student.
+            last_name (int): last name of student.
+            age (int): age of student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of Student instance
+
+        Returns:
+            dict: dictionary representation.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        
+        for i in attrs:
+            try:
+                new_dict[i] = self.__dict__[i]
+            except Exception:
+                pass
+        return new_dict
