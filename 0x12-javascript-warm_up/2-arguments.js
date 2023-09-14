f71fdd60b6525958659a0c04a98dc76a1f3e5404
Serinajefthas <serina.jefthas@gmail.com>
2023-09-14 05:31:58 -0700

    Fix js style errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let numArgs;
-numArgs = process.argv.length;
+const numArgs = process.argv.length;
 
-if (numArgs === 2)
-  console.log("No argument");
-else if (numArgs === 3)
-  console.log("Argument found");
-else
-  console.log("Arguments found");
+if (numArgs === 2) {
+  console.log('No argument');
+} else if (numArgs === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-let arg;
-arg = process.argv[2];
+const arg = process.argv[2];
 
-if (arg === undefined)
-  console.log("No argument");
-else
-	console.log(arg);
+if (arg === undefined) { console.log('No argument'); } else { console.log(arg); }
