67d2bc42ec49be6d3dcd7245ecc3d9be350783ce
Serinajefthas <serina.jefthas@gmail.com>
2023-09-29 07:34:45 -0700

    Add if statement check program is main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print('Body response:')
-    print('    - type:', type(html))
-    print('    - content:', repr(html))
-    print('    - utf8 content:', html.decode('utf-8'))
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as
+    response:
+        html = response.read()
+        print('Body response:')
+        print('    - type:', type(html))
+        print('    - content:', repr(html))
+        print('    - utf8 content:', html.decode('utf-8'))
