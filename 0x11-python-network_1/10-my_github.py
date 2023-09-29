d82cf1d8ed76c26f40d7ba1caace5b20621eb5de
Serina Jefthas <serina.jefthas@gmail.com>
2023-09-29 16:58:17 -0700

    Add task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 # Script takes github username and password and uses github api
 # to display id
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    auth = (sys.argv[1], sys.argv[2])
+    r = requests.get(url, auth=auth)
+    jsonobj = r.json()
+    print(jsonobj.get('id'))
