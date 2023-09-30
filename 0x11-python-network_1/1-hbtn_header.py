69c0c71fffcadd311f06f284341d12b774c71495
Serina Jefthas <serina.jefthas@gmail.com>
2023-09-29 17:07:09 -0700

    Update task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
         x_request_id = response.headers.get('X-Request-Id')
-    print('{}'.format(x_request_id))
+    print(x_request_id)
