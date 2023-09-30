b27a402c0248ef0003fbae1eeb8f8aee71d2be9f
Serina Jefthas <serina.jefthas@gmail.com>
2023-09-30 03:01:49 -0700

    Task 3 updates

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """script displays body of response (decoded utf-8)
 manages HTTPError exceptions and status codes"""
-import urllib.request
-import urllib.error
+from urllib import request, error
 import sys
 
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            html = response.read().decode('utf-8')
-            print(html)
-    except urllib.error.HTTPError as e:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
         print('Error code: ', e.code)
